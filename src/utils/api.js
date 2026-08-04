d684a2b63ddde9efc8ec1b9a6a1c3782fde8b1b4
JJH <sadsnow32@gmail.com>
2026-08-04 23:37:28 +0900

    fix: 특수문자(! ' ( ) * ~) URL 인코딩 추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,10 +13,20 @@ function normalize(query) {
   return query.replace(/\s+/g, '');
 }
 
+/**
+ * URL 경로용 인코딩
+ * encodeURIComponent는 ! ' ( ) * ~ 를 인코딩하지 않아
+ * 일부 서버에서 라우팅 오류가 발생하므로 추가 인코딩
+ */
+function encodeQuery(query) {
+  return encodeURIComponent(normalize(query))
+    .replace(/[!'()*~]/g, c => '%' + c.charCodeAt(0).toString(16).toUpperCase());
+}
+
 export function fetchBySong(query) {
-  return fetcher(`${BASE}/song/${encodeURIComponent(normalize(query))}.json`);
+  return fetcher(`${BASE}/song/${encodeQuery(query)}.json`);
 }
 
 export function fetchBySinger(query) {
-  return fetcher(`${BASE}/singer/${encodeURIComponent(normalize(query))}.json`);
+  return fetcher(`${BASE}/singer/${encodeQuery(query)}.json`);
 }
